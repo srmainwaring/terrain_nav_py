f1879052cf7eb6771d6674401ad94ca283ce6bab
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-03-02 20:34:57 +0000

    dubins_airplane: stub out methods
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- tests/test_dubins_airplane.py
+++ tests/test_dubins_airplane.py
@@ -1,13 +1,53 @@
 import math
 import pytest
 
+from ompl import base as ob
 
-def test_dubins_airplane():
-    from ompl import base as ob
 
+def test_ompl_base_states():
+
+    # R(3) state space
+    r3_ss = ob.RealVectorStateSpace(3)
+    # State
+    r3_s = ob.State(r3_ss)
+    # RealVectorStateInternal
+    r3_s_ref = r3_s()
+    # accessors
+    assert r3_s_ref[0] == 0.0
+    r3_s_ref[0] = 1.0
+    assert r3_s_ref[0] == 1.0
+
+    # SO(2) state space
+    so2_ss = ob.SO2StateSpace()
+    # State
+    so2_s = ob.State(so2_ss)
+    # SO2StateInternal
+    so2_s_ref = so2_s()
+    # accessors
+    assert so2_s_ref.value == 0.0
+    so2_s_ref.value = 1.0
+    assert so2_s_ref.value == 1.0
+
+    # compound state space
+    c_ss = ob.CompoundStateSpace()
+    c_ss.addSubspace(r3_ss, 1.0)
+    c_ss.addSubspace(so2_ss, 1.0)
+    c_ss.lock()
+    # State
+    c_s = ob.State(c_ss)
+    # CompoundStateInternal
+    c_s_ref = c_s()
+
+    # state type conversions
+    r_s = c_s_ref[0]
+    s_s = c_s_ref[1]
+
+    assert r_s[0] == 0.0
+    assert s_s.value == 0.0
+
+
+def test_dubins_airplane_state():
     from terrain_nav_py.dubins_airplane import DubinsAirplaneStateSpace
-
-    # ss = DubinsAirplaneStateSpace()
 
     # compound state space
     c_ss = ob.CompoundStateSpace()
@@ -51,48 +91,19 @@ def test_dubins_airplane():
     assert s.getZ() == 40.0
 
 
-def test_ompl_base():
-    from ompl import base as ob
-    from ompl.base._base import CompoundStateInternal
+def test_dubins_airplane_state_space():
+    from terrain_nav_py.dubins_airplane import DubinsAirplaneStateSpace
 
-    # RV(3) state space
-    rv3_ss = ob.RealVectorStateSpace(3)
-    # State
-    rv3_s = ob.State(rv3_ss)
-    # RealVectorStateInternal
-    rv3_s_ref = rv3_s()
-    # accessors
-    assert rv3_s_ref[0] == 0.0
-    rv3_s_ref[0] = 1.0
-    assert rv3_s_ref[0] == 1.0
+    ss = DubinsAirplaneStateSpace()
+    # TODO: why DubinsAirplaneCompoundSpace6?
+    # assert ss.getName() == "DubinsAirplaneCompoundSpace3"
 
-    # S0(2) state space
-    so2_ss = ob.SO2StateSpace()
-    # State
-    so2_s = ob.State(so2_ss)
-    # S02StateInternal
-    so2_s_ref = so2_s()
-    # accessors
-    assert so2_s_ref.value == 0.0
-    so2_s_ref.value = 1.0
-    assert so2_s_ref.value == 1.0
+    # TODO: fix type
+    assert ss.getType() == ob.StateSpaceType.STATE_SPACE_UNKNOWN
+    # assert ss.getType() == ob.StateSpaceType.STATE_SPACE_SE3
 
-    # compound state space
-    c_ss = ob.CompoundStateSpace()
-    c_ss.addSubspace(rv3_ss, 1.0)
-    c_ss.addSubspace(so2_ss, 1.0)
-    c_ss.lock()
-    # State
-    c_s = ob.State(c_ss)
-    # CompoundStateInternal
-    c_s_ref = c_s()
+    # max extent is pi for null R(3) x SO(2)
+    e = ss.getMaximumExtent()
+    assert e == math.pi
 
-    # state type conversions
-    r_s = c_s_ref[0]
-    s_s = c_s_ref[1]
 
-    assert r_s[0] == 0.0
-    assert s_s.value == 0.0
-
-    # conmpound_state = ob.CompoundState()
-    # so2_state = ob.SO2State()
