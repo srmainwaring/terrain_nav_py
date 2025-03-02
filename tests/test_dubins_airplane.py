0c4ce28988d73e4320a236372058721ae09e1ddc
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-03-02 16:47:15 +0000

    dubins_airplane: initial port of main classes with py-ompl integration
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ tests/test_dubins_airplane.py
@@ -0,0 +1,98 @@
+import math
+import pytest
+
+
+def test_dubins_airplane():
+    from ompl import base as ob
+
+    from terrain_nav_py.dubins_airplane import DubinsAirplaneStateSpace
+
+    # ss = DubinsAirplaneStateSpace()
+
+    # compound state space
+    c_ss = ob.CompoundStateSpace()
+    c_ss.addSubspace(ob.RealVectorStateSpace(3), 1.0)
+    c_ss.addSubspace(ob.SO2StateSpace(), 1.0)
+    c_ss.lock()
+
+    s = DubinsAirplaneStateSpace.DubinsAirplaneState(c_ss)
+    assert s.getX() == 0.0
+    assert s.getY() == 0.0
+    assert s.getZ() == 0.0
+    assert s.getYaw() == 0.0
+
+    s.setX(100.0)
+    s.setY(200.0)
+    s.setZ(50.0)
+    s.setYaw(0.25 * math.pi)
+    assert s.getX() == 100.0
+    assert s.getY() == 200.0
+    assert s.getZ() == 50.0
+    assert s.getYaw() == 0.25 * math.pi
+
+    s.setXYZ(120.0, 210.0, 60.0)
+    assert s.getX() == 120.0
+    assert s.getY() == 210.0
+    assert s.getZ() == 60.0
+
+    s.setXYZYaw(150.0, 230.0, 45.0, 0.5 * math.pi)
+    assert s.getX() == 150.0
+    assert s.getY() == 230.0
+    assert s.getZ() == 45.0
+    assert s.getYaw() == 0.5 * math.pi
+
+    s.addToX(30.0)
+    assert s.getX() == 180.0
+
+    s.addToY(35.0)
+    assert s.getY() == 265.0
+
+    s.addToZ(-5.0)
+    assert s.getZ() == 40.0
+
+
+def test_ompl_base():
+    from ompl import base as ob
+    from ompl.base._base import CompoundStateInternal
+
+    # RV(3) state space
+    rv3_ss = ob.RealVectorStateSpace(3)
+    # State
+    rv3_s = ob.State(rv3_ss)
+    # RealVectorStateInternal
+    rv3_s_ref = rv3_s()
+    # accessors
+    assert rv3_s_ref[0] == 0.0
+    rv3_s_ref[0] = 1.0
+    assert rv3_s_ref[0] == 1.0
+
+    # S0(2) state space
+    so2_ss = ob.SO2StateSpace()
+    # State
+    so2_s = ob.State(so2_ss)
+    # S02StateInternal
+    so2_s_ref = so2_s()
+    # accessors
+    assert so2_s_ref.value == 0.0
+    so2_s_ref.value = 1.0
+    assert so2_s_ref.value == 1.0
+
+    # compound state space
+    c_ss = ob.CompoundStateSpace()
+    c_ss.addSubspace(rv3_ss, 1.0)
+    c_ss.addSubspace(so2_ss, 1.0)
+    c_ss.lock()
+    # State
+    c_s = ob.State(c_ss)
+    # CompoundStateInternal
+    c_s_ref = c_s()
+
+    # state type conversions
+    r_s = c_s_ref[0]
+    s_s = c_s_ref[1]
+
+    assert r_s[0] == 0.0
+    assert s_s.value == 0.0
+
+    # conmpound_state = ob.CompoundState()
+    # so2_state = ob.SO2State()
